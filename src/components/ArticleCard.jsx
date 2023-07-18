b28ced985b59f6e6f5a838b20d58c609295ba913
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-07-18 11:48:35 +0100

    Added improvements

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,8 +2,8 @@ const ArticleCard = props => {
   const {article_img_url, author, comment_count, created_at, title, topic} = props;
   return (
     <div className="article-card">
-      <div className="article-main">
-        <div className="article-header">
+      <main className="article-main">
+        <header className="article-header">
           <div className="names">
             <h3>{title}</h3>
             <p className="author-name">By: {author}</p>
@@ -12,12 +12,12 @@ const ArticleCard = props => {
             <p className="date-posted">{created_at}</p>
             <p className="topic">{topic}</p>
           </div>
-        </div>
-        <div className="article-footer">
+        </header>
+        <footer className="article-footer">
           <p className="likes-count">0 Likes</p>
           <p className="comment-count">{comment_count} Comments</p>
-        </div>
-      </div>
+        </footer>
+      </main>
       <img src={article_img_url} alt="article image" />
     </div>
   )
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -5,18 +5,19 @@ import { getArticles } from "./utils/api";
 
 const ArticlesList = () => {
   const [articles, setArticles] = useState([])
-  
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
     getArticles().then(({articles}) => {
       setArticles(articles)
-      console.log(articles)
+      setIsLoading(false)
     })
   }, [])
 
   return (
     <section className="articles-list">
       <h2>Now trending</h2>
-      {articles.map(article => {
+      {isLoading ? <p className="loading">Loading page...</p> : articles.map(article => {
         return <ArticleCard 
         key={article.article_id} 
         article_img_url={article.article_img_url}
