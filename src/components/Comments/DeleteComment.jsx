6f2a222a667f3ca7a786ce7d1d35286e05c7105d
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-07-21 12:11:33 +0100

    now disables button while request is ongoing

--- src/components/Comments/DeleteComment.jsx
+++ src/components/Comments/DeleteComment.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import { deleteComment } from "../utils/api";
 
 const DeleteComment = props => {
   const { setComments, comment_id } = props;
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   const handleClick = () => {
+    setIsLoading(true)
     deleteComment(comment_id)
     .then(() => {
+      setIsLoading(false)
       setComments(comments => comments.filter(comment => comment.comment_id !== comment_id))
     })
+    .catch(() => {
+      setIsLoading(false)
+      setIsError(true)
+    })
+
   }
 
   return (
-    <button onClick={handleClick} className="delete-comment">Delete comment</button>
+    <>
+      {isError && <p className="err-msg">Error Occured: Please try again later</p>}
+      <button disabled={isLoading} onClick={handleClick} className="delete-comment">Delete comment</button>
+    </>
   )
 }
 
