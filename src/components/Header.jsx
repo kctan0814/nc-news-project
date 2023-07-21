49a3b26bfa0b42f9ca5820795ac7fb5b7653e146
Kc Tan <kctan@Kcs-MacBook-Air.local>
2023-07-21 13:42:50 +0100

    Added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/logo.jpg'
 
 const Header = () => {
   return (
     <Link className="no-decoration" to="/">
       <header className="header">
-        <img src="/logo.jpg" alt="Logo" className="logo"/>
+        <img src={logo} alt="Logo" className="logo"/>
         <h1>NC News</h1>
         <p>delivering the hottest scoops</p>
       </header>
